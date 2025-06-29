752fcdeed38cee7f56811cf1406890602997a383
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-06-29 14:49:36 +0300

    Fix issue where JSON files were not being read from directory

--- src/utils/cache.py
+++ src/utils/cache.py
@@ -10,15 +10,15 @@ class Cache:
     """Read and write cached Udemy links to JSON files."""
 
     def __init__(self) -> None:
-        self.data_dir: Path = Path(
-            __file__).parent.parent.parent / 'data' / FORMATTED_DATE
-        self.data_dir.mkdir(parents=True, exist_ok=True)
+        self.json_dir: Path = Path(
+            __file__).parent.parent.parent / 'data' / FORMATTED_DATE / 'json'
+        self.json_dir.mkdir(parents=True, exist_ok=True)
         self.logger = setup_logging()
         self.udemy_urls: set[str] = set()
 
     def read_json(self) -> None:
         """Parse cached Udemy links with coupons from JSON files."""
-        file_path: Path = self.data_dir / 'udemy.json'
+        file_path: Path = self.json_dir / 'udemy.json'
         if file_path.exists():
             with file_path.open('r', encoding='utf-8') as f:
                 self.udemy_urls = set(json.load(f))
@@ -27,7 +27,7 @@ class Cache:
 
     def write_json(self, data: set[str]) -> None:
         """Write output to JSON file in a 'data' directory inside root directory."""
-        file_path: Path = self.data_dir / 'json/udemy.json'
+        file_path: Path = self.json_dir / 'udemy.json'
         with file_path.open('w', encoding='utf-8') as f:
             json.dump(list(data), f,
                       ensure_ascii=False, indent=4)
