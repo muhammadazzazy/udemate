6e2fceab30f248b3073b88523980beeececb9dc9
Muhammad Azzazy <muhammad-azzazy@aucegypt.edu>
2025-12-14 21:09:41 +0200

    Remove regex and string slicing from Udemy URL cleaning

--- src/bot/spider.py
+++ src/bot/spider.py
@@ -1,18 +1,17 @@
 """Encapsulate common attributes and functionality between middleman spiders."""
-import re
-import urllib.parse as urlparse
+from urllib.parse import ParseResult, urlparse, parse_qs, urlencode, urlunparse
 from abc import ABC, abstractmethod
 
 from gotify import Gotify
 
-from utils.config import BotConfig
+from utils.config import SpiderConfig
 from utils.logger import setup_logging
 
 
 class Spider(ABC):
     """Encapsulates shared attributes and abstract methods for intermediary scrapers."""
 
-    def __init__(self, *, urls: list[str], gotify: Gotify, config: BotConfig) -> None:
+    def __init__(self, *, urls: list[str], gotify: Gotify, config: SpiderConfig) -> None:
         self.urls = urls
         self.gotify = gotify
         self.retries = config.retries
@@ -21,19 +20,23 @@ class Spider(ABC):
         self.logger = setup_logging()
 
     def clean(self, url: str) -> str:
-        """Return clean Udemy link without tracking parameters."""
-        parsed = urlparse.urlparse(url)
-        params = urlparse.parse_qs(parsed.query)
+        """Return clean Udemy link with coupon code."""
+        parsed: ParseResult = urlparse(url)
+        params: dict[str, list[str]] = parse_qs(parsed.query)
         udemy_url: str = params.get('u', [url])[0]
-        match = re.search(r'couponCode=([^&]+)', udemy_url)
-        question_mark_idx: int = udemy_url.find('?')
-        if match:
-            coupon_code = match.group(1)
-            if question_mark_idx != -1:
-                udemy_url = udemy_url[:udemy_url.find(
-                    '?')+1] + 'couponCode=' + coupon_code
-                return udemy_url
-        return udemy_url[:question_mark_idx] if question_mark_idx != -1 else udemy_url
+        udemy_parsed: ParseResult = urlparse(udemy_url)
+
+        udemy_params: dict[str, list[str]] = parse_qs(udemy_parsed.query)
+
+        clean_params: dict[str, list[str]] = {}
+        if 'couponCode' in udemy_params:
+            clean_params['couponCode'] = udemy_params['couponCode']
+
+        clean_query: str = urlencode(clean_params, doseq=True)
+
+        return urlunparse(
+            udemy_parsed._replace(query=clean_query)
+        )
 
     @abstractmethod
     def transform(self, url: str) -> str | None:
